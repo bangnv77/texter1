d7c4628a31c3236f93f2e19e34afdc7d07360c7b
Bang Nguyen <bangnv77@yahoo.com>
2016-09-27 00:22:13 -0700

    add AJAX to new message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,16 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @messages = Message.all
     @message = Message.new(message_params)
     if @message.save
       flash[:notice] = "your message was sent!"
-      redirect_to messages_path
+      respond_to do |format|
+        format.html { redirect_to messages_path }
+        format.js
+      end
     else
+      flash[:notice] = 'Message unable to be saved'
       render 'new'
     end
   end
