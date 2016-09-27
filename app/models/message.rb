7357f9688a0e9b229e0804082a674e5376547e13
Bang Nguyen <bangnv77@yahoo.com>
2016-09-26 18:12:15 -0700

    create .env file to hide sensitive info

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,8 +7,8 @@ private
     response = RestClient::Request.new(
       :method => :post,
       :url => 'https://api.twilio.com/2010-04-01/Accounts/AC032a930f87c16f039f03517e32d6b7ff/Messages.json',
-      :user => 'AC032a930f87c16f039f03517e32d6b7ff',
-      :password => '072c3217d4934875a75e1ed192c68c97',
+      :user => ENV['TWILIO_ACCOUNT_SID'],
+      :password => ENV['TWILIO_AUTH_TOKEN'],
       :payload => { :Body => body,
                     :From => from,
                     :To => to }
